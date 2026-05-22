16546d85d56205dc43d43047a17467e782684c36
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-05-22 10:03:47 +0530

    Add files via upload

---
+++ C2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	int a=11;
+	
+	while(a<=19)
+    {
+        cout << a << endl;
+        a+=2;
+    }
+    
+    return 0;
+    
+}
