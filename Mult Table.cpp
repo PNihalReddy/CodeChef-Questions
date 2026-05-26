f36a46199c6483a91d216e068e35fabb0491fcb3
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-05-26 10:51:55 +0530

    Add files via upload

---
+++ Mult Table.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  
+    int n;
+    cin >> n;
+    
+    int i;
+    for(i=1;i<=10;i=i+1) 
+    {
+        cout << n << " x " << i << " = " << n*i << endl;
+    }
+    
+    return 0;
+    
+}
