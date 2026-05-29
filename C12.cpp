5f908aaab126c78f3973bba3b3ac322f97529b1d
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-05-29 11:00:03 +0530

    Add files via upload

---
+++ C12.cpp
@@ -0,0 +1,16 @@
+//How many times will "C++" be printed by this code?
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+  for (int i = 0 ; i <= 5 ; i = i + 2) 
+  {
+    cout << "C++"<< endl;
+  }
+  
+  return 0;
+  
+}
