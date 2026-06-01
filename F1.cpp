2137fb2dec8c38a7030c03ba3a502a31c33b6abc
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-06-01 10:58:39 +0530

    Add files via upload

---
+++ F1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int a, b;
+    cin >> a >> b;
+    cout << (a * a + 2 * a * b + b * b) << endl;
+    cout << (a + b) << endl;
+}
+
+int main() 
+{
+    solve();
+    solve();
+    solve();
+    
+    return 0;
+}
