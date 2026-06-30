590334ea234f3b2ff4a459cdb9f31c7a41838edb
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-06-30 12:40:35 +0530

    Add files via upload

---
+++ Single Number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int singleNumber(vector<int>& nums) 
+{
+    int ans=0;
+
+    for(int val: nums)
+    {
+        ans^=val;
+    }    
+    
+    return ans;
+}
+
+int main() 
+{
+    vector<int> nums = {4, 1, 2, 1, 2};
+    
+    int res = singleNumber(nums);
+    
+    cout << "The single number is: " << res << endl;
+    
+    return 0;
+    
+}
